1afcaba110a32325fef6eceb8a1f2f1b55e4a65d
rust404 <rust404@outlook.com>
2021-06-22 19:39:49 +0800

    feat: add linter

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  extends: [require.resolve('@umijs/fabric/dist/eslint')],
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+  },
+}
---
+++ packages/module1/src/index.ts
@@ -0,0 +1,7 @@
+const a: number = 1
+const b = 2
+function test(a: number, b: number) {
+  console.log(a, b)
+}
+console.log(a)
+test(1, 2)
