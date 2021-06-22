97c9b792826a12e7f86f575c38b21f7f23cb9a0b
rust404 <rust404@outlook.com>
2021-06-22 10:28:46 +0800

    init

---
+++ packages/module1/lib/index.js
@@ -0,0 +1,11 @@
+'use strict';
+const module2 = require('module2')
+
+module.exports = module1;
+
+function module1() {
+  console.log('module1')
+  module2()
+}
+
+module1()
---
+++ packages/module2/lib/index.js
@@ -0,0 +1,10 @@
+'use strict';
+const _ = require('lodash')
+
+module.exports = module2;
+
+function module2() {
+    // TODO
+  console.log('module2')
+  console.log(_.add(1,2))
+}
